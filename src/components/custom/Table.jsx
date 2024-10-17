aed73ddbe89e7d41707f4bcc8a7ebf34d735ed2e
Dineshdk1705 <dnsh555g@gmail.com>
2024-10-17 08:30:42 +0530

    fix responsiveness for table

--- src/components/custom/Table.jsx
+++ src/components/custom/Table.jsx
@@ -5,14 +5,24 @@ const Table = () => {
   return (
     <div className="pt-4 pb-8 w-full">
       <div className="overflow-x-auto w-full">
-        <table className="min-w-full border-collapse text-sm">
+        <table className="min-w-[640px] sm:min-w-full border-collapse text-sm">
           <thead>
             <tr className="bg-transparent border-b-[1px] border-gray-800 text-gray-400">
-              <th className="pr-2 py-2 text-left font-normal">Order number</th>
-              <th className="pr-2 py-2 text-left font-normal">Purchase date</th>
-              <th className="pr-2 py-2 text-left font-normal">Customer</th>
-              <th className="pr-2 py-2 text-left font-normal">Event</th>
-              <th className="pr-2 py-2 text-right font-normal">Amount</th>
+              <th className="pr-2 py-2 text-left font-normal whitespace-nowrap">
+                Order number
+              </th>
+              <th className="pr-2 py-2 text-left font-normal whitespace-nowrap">
+                Purchase date
+              </th>
+              <th className="pr-2 py-2 text-left font-normal whitespace-nowrap">
+                Customer
+              </th>
+              <th className="pr-2 py-2 text-left font-normal whitespace-nowrap">
+                Event
+              </th>
+              <th className="pr-2 py-2 text-right font-normal whitespace-nowrap">
+                Amount
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -31,10 +41,12 @@ const Table = () => {
                   key={orderNumber}
                   className="bg-transparent border-b-[1px] border-gray-800"
                 >
-                  <td className="pr-2 py-5">{orderNumber}</td>
-                  <td className="pr-2 py-4 text-gray-500">{purchaseDate}</td>
-                  <td className="pr-2 py-4">{customer}</td>
-                  <td className="pr-2 py-4 flex gap-2">
+                  <td className="pr-2 py-5 whitespace-nowrap">{orderNumber}</td>
+                  <td className="pr-2 py-4 text-gray-500 whitespace-nowrap">
+                    {purchaseDate}
+                  </td>
+                  <td className="pr-2 py-4 whitespace-nowrap">{customer}</td>
+                  <td className="pr-2 py-4 flex gap-2 whitespace-nowrap">
                     <img
                       src={imgSrc}
                       alt="Event"
@@ -42,7 +54,9 @@ const Table = () => {
                     />
                     {event}
                   </td>
-                  <td className="pr-2 py-4 text-right">{`${currency}${amount}`}</td>
+                  <td className="pr-2 py-4 text-right whitespace-nowrap">
+                    {`${currency}${amount}`}
+                  </td>
                 </tr>
               );
             })}
